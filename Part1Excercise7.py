f4725778757a44880c976287d11894ed2537231d
tirupathiakiti <tirupath@gmail.com>
2018-05-16 21:37:08 -0400

    print ony extension

---
+++ Part1Excercise7.py
@@ -0,0 +1,4 @@
+#Write a Python program to accept a filename from the user and print the extension of that
+FileName = input(' provide file name with extension : ')
+FileName_ext = FileName.split(".")
+print (' Provided filename is :' + FileName + 'and extension is :'+ repr(FileName_ext[-1]))
