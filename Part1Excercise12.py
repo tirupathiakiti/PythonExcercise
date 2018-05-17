aacf4911122f85fb898660cb416840842341d7d2
tirupathiakiti <tirupath@gmail.com>
2018-05-17 08:55:09 -0400

    Print current calendar month

---
+++ Part1Excercise12.py
@@ -0,0 +1,8 @@
+#Write a Python program to print the calendar of a given month and year.
+#Note : Use 'calendar' module.
+
+import calendar
+y = int(input ('Enter year: '))
+m = int(input ('Enter Month: '))
+
+print(calendar.month(y,m))
