2ca7c01694fb3a35d61b9b461159ce0845e741a1
tirupathiakiti <tirupath@gmail.com>
2018-05-16 21:42:50 -0400

    print only extension

--- Part1Excercise7.py
+++ Part1Excercise7.py
@@ -1,4 +1,4 @@
 #Write a Python program to accept a filename from the user and print the extension of that
 FileName = input(' provide file name with extension : ')
 FileName_ext = FileName.split(".")
-print (' Provided filename is :' + FileName + 'and extension is :'+ repr(FileName_ext[-1]))
+print (' Provided filename is :' + FileName + 'and extension is :'+ (FileName_ext[-1]))
