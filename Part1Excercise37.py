bac48a635d997b3d1e8e1fdfd57074e6185c3172
tirupathiakiti <tirupath@gmail.com>
2018-05-21 10:33:37 -0400

    print in separate lines

---
+++ Part1Excercise37.py
@@ -0,0 +1,5 @@
+#Write a Python program to display your details like name, age, address in three different lines
+Name = input('Enter Name')
+Age = input('Enter age')
+Address = input('Enter address')
+print("Name: {}\nAge: {}\nAddress: {}".format(Name, Age, Address))
