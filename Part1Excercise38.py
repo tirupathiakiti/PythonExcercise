cb5b7c6d00bb6d44b6c1860d96581396a0c94fce
tirupathiakiti <tirupath@gmail.com>
2018-05-21 12:58:10 -0400

    multiplication ints

---
+++ Part1Excercise38.py
@@ -0,0 +1,7 @@
+#Write a Python program to solve (x + y) * (x + y). Go to the editor
+#Test Data : x = 4, y = 3
+#Expected Output : (4 + 3) ^ 2) = 49
+x = int(input( 'Enter Value: '))
+y = int(input ('Enter Value: '))
+z = ((x+y)*(x+y))
+print (z)
