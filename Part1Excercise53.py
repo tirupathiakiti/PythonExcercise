354861cad02efd7b0c934176acacc6e4643b5537
tirupathiakiti <tirupath@gmail.com>
2018-05-22 13:14:16 -0400

    Print Environment

---
+++ Part1Excercise53.py
@@ -0,0 +1,10 @@
+#Write a python program to access environment variables
+import os
+
+print('*------------------------------*')
+print(os.environ)
+print('*------------------------------*')
+print(os.environ['HOME'])
+print('*------------------------------*')
+print(os.environ['PATH'])
+print('*------------------------------*')
