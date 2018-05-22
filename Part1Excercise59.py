1d943c9d67f867958bafb6bfe6c831800f3104ce
tirupathiakiti <tirupath@gmail.com>
2018-05-22 15:50:23 -0400

    Convert Height into CM

---
+++ Part1Excercise59.py
@@ -0,0 +1,9 @@
+#Write a Python program to convert height (in feet and inches) to centimeters
+print(" Input your Height : ")
+h_ft = int(input("Feet: "))
+h_inch =int(input("Inches: "))
+
+h_inch +=h_ft *12
+h_cm = round(h_inch * 2.54,1)
+
+print("Your height is : %d cm. " %h_cm)
