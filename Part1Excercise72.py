a92816f224eb6010a628c23a3958c1d47c25d037
tirupathiakiti <tirupath@gmail.com>
2018-05-24 22:58:55 -0400

    math module

---
+++ Part1Excercise72.py
@@ -0,0 +1,6 @@
+#Write a Python program to get the details of math module
+# Imports the math module
+import math            
+#Sets everything to a list of math module
+math_ls = dir(math) # 
+print(math_ls)
